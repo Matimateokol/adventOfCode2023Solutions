166b10f0613abdc08335386879f1fe16c6449871
Matimateokol <58779750+Matimateokol@users.noreply.github.com>
2023-12-02 19:17:41 +0100

    initial commit

---
+++ src/main/java/pl/matimateokol/Main.java
@@ -0,0 +1,9 @@
+package pl.matimateokol;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("""
+            Stub message. In order to see solutions for each day, go to the relevant package and execute Main.main(...) method from there.
+        """);
+    }
+}
