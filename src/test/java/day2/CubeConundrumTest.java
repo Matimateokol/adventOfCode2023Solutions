c6f2f988d7c8a75762511be2ba1f4399433f41c3
Matimateokol <58779750+Matimateokol@users.noreply.github.com>
2023-12-02 19:32:34 +0100

    Make small code refactoring

--- src/test/java/day2/CubeConundrumTest.java
+++ src/test/java/day2/CubeConundrumTest.java
@@ -10,19 +10,19 @@ class CubeConundrumTest {
     void checkIfGamePossible() {
 
         boolean isGamePossible = CubeConundrum.checkIfGamePossible("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green");
-        assertEquals(true, isGamePossible);
+        assertTrue(isGamePossible);
 
         isGamePossible = CubeConundrum.checkIfGamePossible("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue");
-        assertEquals(true, isGamePossible);
+        assertTrue(isGamePossible);
 
         isGamePossible = CubeConundrum.checkIfGamePossible("Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red");
-        assertEquals(false, isGamePossible);
+        assertFalse(isGamePossible);
 
         isGamePossible = CubeConundrum.checkIfGamePossible("Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red");
-        assertEquals(false, isGamePossible);
+        assertFalse(isGamePossible);
 
         isGamePossible = CubeConundrum.checkIfGamePossible("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green");
-        assertEquals(true, isGamePossible);
+        assertTrue(isGamePossible);
     }
 
     @Test
